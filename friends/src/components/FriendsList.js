6754628e2b9f08d3ad821d1f16c5c35488e35f13
croy <roy.christie95@gmail.com>
2019-01-31 17:21:48 -0600

    Accomplished some basic styling and imported a logo

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const FriendListWrapper = styled.div`
+    border: 5px solid #EAB3A7;
+    width: 100%;
+`
+
 const FriendWrapper = styled.div`
-    border: 1px solid red;
+    border: 1px solid #FDE1D4;
+    text-align: left;
+    padding-left: 2%;
+    padding-top: 2%;
+    padding-bottom: 2%;
+    background-color: #384C39;
+    color: #EFEEED;
+    font-size: 16px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const DeleteLogo = styled.img`
+    margin-right: 2%;
 `
 
 function FriendsList(props){
     return(
-        <div className="friendWrapper">
+        <FriendListWrapper>
             {props.friends.map(friend => (
-                <FriendWrapper key={friend.id}>{friend.name}</FriendWrapper>
+                <FriendWrapper key={friend.id}>
+                {friend.name}
+                <DeleteLogo src="https://img.icons8.com/office/16/000000/cancel.png"></DeleteLogo>
+                </FriendWrapper>
             ))}
-        </div>
+        </FriendListWrapper>
     );
 }
 
